63a701f5d5f0f173d9ca5f259b6ee51ea23a1e7d
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2021-04-23 15:38:22 +0100

    updates app.js

--- app.js
+++ app.js
@@ -46,7 +46,6 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.locals.title = 'Food Project';
 
 
-
 const index = require('./routes/index');
 app.use('/', index);
 
