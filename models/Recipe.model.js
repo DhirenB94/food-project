f5ce81a9ea6b112af6d5f3c05c9f18b3dc79122a
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2021-04-23 15:16:55 +0100

    user and recipe model done, seed.js started

---
+++ models/Recipe.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose;
+
+const recipeSchema = new Schema ({
+  pictureUrl: String,
+  name: {
+    type: String,
+    required: true
+  },
+  cuisine: String,
+  prepTime: {
+    type: String,
+    required: true
+  },//x mins
+  cookTime: {
+    type: String,
+    required: true
+  },
+  serves: {
+    type: Number,
+    required: true
+  },
+  ingredients: {
+    type: [String],
+    required: true
+  },
+  method: {
+    type: [ String ],
+    required: true
+  },
+  suitable: {
+    type: String,
+    enum : ['Vegetarian', 'Vegan', 'Lactose free', 'Gluten free']
+  },
+  allergy: {
+    type: String,
+    enum : ['Nuts', 'Seafood'],
+  }
+
+// by: {{username}}
+
+})
+module.exports = model("Recipe", recipeSchema);
