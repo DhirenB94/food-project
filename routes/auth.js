36e858488a7ebf3750a257d12ed914f6d596e98d
patricia <patriciaferreirarocha25@gmail.com>
2021-04-26 17:52:33 +0100

    nutrition.js completed

--- routes/auth.js
+++ routes/auth.js
@@ -40,24 +40,15 @@ router.post('/login', async (req, res) => {
 })
 
 router.get('/signup', (req, res) => {
-  res.render('auth/login');
+  res.render('auth/signup');
 });
 
 router.post('/signup', async (req, res) => {
   const { username , password} = req.body;
 
   if(username === '' || password === '') {
-    res.render('auth/login', {
+    res.render('auth/signup', {
       errorMessage: 'Indicate username and password'
-    })
-    return;
-  }
-
-  const user = await User.findOne({ username: username });
-
-  if(user !== null) {
-    res.render('auth/login', {
-      errorMessage: 'Username already exists'
     })
     return;
   }
@@ -68,13 +59,22 @@ router.post('/signup', async (req, res) => {
     { errorMessage: 'Password is too weak' })
     return;
   }*/
+
+  const user = await User.findOne({ username: username });
+
+  if(user !== null) {
+    res.render('auth/signup', {
+      errorMessage: 'Username already exists'
+    })
+    return;
+  }
+
   const saltRounds = 10;
   const salt = bcrypt.genSaltSync(saltRounds);
   const hashedPassword = bcrypt.hashSync(password, salt);
   
   await User.create({
     username,
-    email,
     password: hashedPassword
   });
 
--- routes/nutrients.js
+++ routes/nutrients.js
@@ -1,8 +1,26 @@
+
+const express = require('express');
+const router  = express.Router();
 const { FoodDatabaseClient } = require('edamam-api');
- 
+
+
 const client = new FoodDatabaseClient({
   appId: process.env.API_ID,
   appKey: process.env.API_KEY
 });
 
-const nutrients = client.getNutrients({ ingredients: [{ quantity, measureURI, foodId }]});
+router.get('/food-search', async (req, res, next) => {    
+  try {
+    let foodSearch = req.query.foodSearch;
+    const food = await client.search({query: foodSearch});
+
+    res.render('food-search-results', {foods: food.hints});
+    
+    console.log(food.hints.label);
+
+  } catch(e) {
+  console.log(`nutrient error ${e}`)
+  }
+});
+
+module.exports = router;
